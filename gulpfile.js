552d62371090e9903349ab317a33e2e675bd67ba
Unknown <fwirwicki@gmail.com>
2017-03-17 14:19:37 +0100

    Add SCSS support

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,40 @@
-var gulp = require('gulp'),
-    plumber = require('gulp-plumber'),
-    rename = require('gulp-rename');
-var autoprefixer = require('gulp-autoprefixer');
-var browserSync = require('browser-sync');
+"use strict";
 
-gulp.task('browser-sync', function() {
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var rename = require("gulp-rename");
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+var browserSync = require("browser-sync");
+
+gulp.task("browser-sync", function() {
   browserSync({
     server: {
-       baseDir: "app/"
+       baseDir: "./"
     }
   });
 });
 
-gulp.task('bs-reload', function () {
+gulp.task("bs-reload", function () {
   browserSync.reload();
 });
 
 
-gulp.task('styles', function(){
-  gulp.src(['app/css/**/*.css'])
+gulp.task("styles", function(){
+  gulp.src(["app/css/**/*.scss"])
     .pipe(plumber({
       errorHandler: function (error) {
         console.log(error.message);
-        this.emit('end');
+        this.emit("end");
     }}))
-    .pipe(autoprefixer('last 2 versions'))
-    .pipe(gulp.dest('dist/styles/'))
+    .pipe(sass().on("error", sass.logError))
+    .pipe(autoprefixer("last 2 versions"))
+    .pipe(gulp.dest("dist/"))
     .pipe(browserSync.reload({stream:true}))
 });
 
 
-gulp.task('default', ['browser-sync'], function(){
-  gulp.watch("app/css/**/*.css", ['styles']);
-  gulp.watch("app/**/*.html", ['bs-reload']);
-  gulp.watch("*.html", ['bs-reload']);
+gulp.task("default", ["styles", "browser-sync"], function(){
+  gulp.watch("app/css/**/*.scss", ["styles"]);
+  gulp.watch("*.html", ["bs-reload"]);
 });
